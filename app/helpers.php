8c06db136b416b48577265b96a8d75b0fb824889
Ahmed-Ri <121891937+Ahmed-Ri@users.noreply.github.com>
2024-02-25 18:46:43 +0100

    shop-radar

--- app/helpers.php
+++
@@ -1,5 +0,0 @@
-<?php
-function getPrice($prix){
-    $prixTTC=floatval($prix);
-    return number_format($prixTTC, 2, ',', '') . ' €';
-}
--- resources/views/partials/session-messages.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- Affichage des messages de succès --}}
-@if(session('success'))
-    <div class="alert alert-success"> {{ session('success') }} </div>
-@endif
-
-{{-- Affichage des messages d'erreur --}}
-@if(session('error'))
-    <div class="alert alert-danger">{{ session('error') }}</div>
-@endif
